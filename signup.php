bc5b07ec18b19f71fc8b080fedf0ac6df0ad25d8
lasindu77 <lasindupamod77@gmail.com>
2024-03-17 21:43:59 +0530

    first commit

---
+++ signup.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/animations.css">  
+    <link rel="stylesheet" href="css/main.css">  
+    <link rel="stylesheet" href="css/signup.css">
+        
+    <title>Sign Up</title>
+    <style>
+        body {
+            background-image: url('img/i3.jpg'); /* Replace 'path/to/your/image.jpg' with the actual path to your background image */
+            background-size: cover; /* This property ensures that the background image covers the entire body */
+            background-repeat: no-repeat; /* This property prevents the background image from repeating */
+        }
+    </style>
+    
+</head>
+<body>
+<?php
+
+class SessionManager {
+    private static $instance;
+
+    private function __construct() {
+        session_start();
+    }
+
+    public static function getInstance() {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function setPersonalDetails($personalDetails) {
+        $_SESSION["personal"] = $personalDetails;
+    }
+
+    public function getPersonalDetails() {
+        return $_SESSION["personal"] ?? [];
+    }
+
+    // Add other session-related methods here as needed
+}
+
+// Usage:
+$sessionManager = SessionManager::getInstance();
+
+// Clear session variables
+$sessionManager->setPersonalDetails([]);
+
+// Other session operations...
+
+if ($_POST) {
+    $personalDetails = [
+        'fname' => $_POST['fname'],
+        'lname' => $_POST['lname'],
+        'address' => $_POST['address'],
+        'nic' => $_POST['nic'],
+        'dob' => $_POST['dob']
+    ];
+
+    // Store personal details in session
+    $sessionManager->setPersonalDetails($personalDetails);
+
+    header("location: create-account.php");
+}
+
+?>
+
+
+
+    <center>
+    <div class="container" style="background-color: #090d14; border:0px;">
+        <table border="0">
+            <tr>
+                <td colspan="2">
+                    <p class="header-text" style="color: #f5f7fa;">Let's Get Started</p>
+                    <p class="sub-text">Add Your Personal Details to Continue</p>
+                </td>
+            </tr>
+            <tr>
+                <form action="" method="POST" >
+                <td class="label-td" colspan="2">
+                    <label for="name" class="form-label">Name: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td">
+                    <input type="text" name="fname" class="input-text" placeholder="First Name" required>
+                </td>
+                <td class="label-td">
+                    <input type="text" name="lname" class="input-text" placeholder="Last Name" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="address" class="form-label">Address: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="text" name="address" class="input-text" placeholder="Address" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="nic" class="form-label">NIC: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="text" name="nic" class="input-text" placeholder="NIC Number" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="dob" class="form-label">Date of Birth: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="date" name="dob" class="input-text" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <input type="reset" value="Reset" class="login-btn btn-primary-soft btn" >
+                </td>
+                <td>
+                    <input type="submit" value="Next" class="login-btn btn-primary btn">
+                </td>
+
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <br>
+                    <label for="" class="sub-text" style="font-weight: 280;">Already have an account&#63; </label>
+                    <a href="login.php" class="hover-link1 non-style-link">Login</a>
+                    <br><br><br>
+                </td>
+            </tr>
+
+                    </form>
+            </tr>
+        </table>
+
+    </div>
+</center>
+</body>
+</html>
